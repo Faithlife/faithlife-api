cff1e1ef778f017782e2e34dfc015e46e9a8b2bb
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2015-07-21 16:48:37 -0700

    Update faithlife-api to use proxy-client.

--- test/accounts.js
+++ test/accounts.js
@@ -1,13 +1,5 @@
 var assert = require('assert');
-var faithlifeOAuth = require('faithlife-oauth');
-var mach = require('mach');
 var lib = require('../lib');
-var TOKEN = '491D486DD7C22BAEC44A06E802272139E6A10D62';
-var SECRET = '839A6F206D70619D45F204FD2CF7E35E6AEEFFDD';
-var consumer = faithlifeOAuth.createConsumer({
-  token: TOKEN,
-  secret: SECRET
-});
 
 /**
  * The Client
@@ -18,62 +10,7 @@ var client = lib.Accounts.createClient();
  * getLoggedInUser
  */
 client.getLoggedInUser()
-  .then(function (response) {
-    // 1. Should respond with a 200.
-    assert.equal(response.status, 200, 'Non-success status code.');
-
-    // 2. Should respond with JSON.
-    assert.ok(response.content && typeof response.content === 'object', 'Missing anonymous response body.');
-
-    // 3. Should respond with a -1 ID if no user is logged-in.
-    assert.deepEqual(
-      response.content,
-      { id: -1 },
-      'Invalid anonymous response body.'
-    );
-  })
-  .done();
-
-// TODO(schoon) - Automate the sign-in form submission and test while
-// authenticated.
-
-/*
- * The Proxy
- */
-var app = mach.stack();
-app.use(mach.session, {
-  secret: 'test',
-  store: mach.session.MemoryStore()
-});
-consumer.mount(app, '/oauth');
-app.map('/api', function (subapp) {
-  subapp.use(consumer.generateMiddleware());
-
-  client.mount(subapp, '/accounts');
-});
-var server = mach.serve(app, process.env.PORT || 22222);
-
-/**
- * getLoggedInUser
- */
-var proxy = lib.Accounts.createClient({ rootUrl: 'http://127.0.0.1:22222/api/accounts' });
-
-proxy.getLoggedInUser()
-  .then(function (response) {
-    // 1. Should respond with a 200.
-    assert.equal(response.status, 200, 'Non-success status code.');
-
-    // 2. Should respond with JSON.
-    assert.ok(response.content && typeof response.content === 'object', 'Missing anonymous response body.');
-
-    // 3. Should respond with a -1 ID if no user is logged-in.
-    assert.deepEqual(
-      response.content,
-      { id: -1 },
-      'Invalid anonymous response body.'
-    );
-  })
-  .then(function () {
-    server.close();
+  .then(function (user) {
+    assert.equal(user, null, 'Did not assert.');
   })
   .done();
